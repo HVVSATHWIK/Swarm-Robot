287e3a5b53d8d36793932ea561a9842894b76671
Veer <146707877+HVVSATHWIK@users.noreply.github.com>
2025-03-07 01:36:05 +0530

    Add files via upload

---
+++ controllers/obstacle_avoidance/build/release/obstacle_avoidance.d
@@ -0,0 +1,6 @@
+build/release/obstacle_avoidance.o: obstacle_avoidance.cpp \
+ C:\Users\Veerendranath\AppData\Local\Programs\Webots/include/controller/cpp/webots/DistanceSensor.hpp \
+ C:\Users\Veerendranath\AppData\Local\Programs\Webots/include/controller/cpp/webots/Device.hpp \
+ C:/Users/Veerendranath/AppData/Local/Programs/Webots/include/controller/c/webots/types.h \
+ C:\Users\Veerendranath\AppData\Local\Programs\Webots/include/controller/cpp/webots/Motor.hpp \
+ C:\Users\Veerendranath\AppData\Local\Programs\Webots/include/controller/cpp/webots/Robot.hpp
